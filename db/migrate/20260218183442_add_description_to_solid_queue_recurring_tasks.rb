7bcd0ac362b8a0339a0bd5fb1a0fbc68e5873b1f
lomarrojasm <omar.rojas@ligoconsulting.com>
2026-02-18 13:01:23 -0600

    Fix: Reconstruye tablas SolidQueue con schema correcto v1

--- db/migrate/20260218183442_add_description_to_solid_queue_recurring_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToSolidQueueRecurringTasks < ActiveRecord::Migration[8.0]
-  def change
-    add_column :solid_queue_recurring_tasks, :description, :string
-  end
-end
--- db/migrate/20260218183946_add_static_to_solid_queue_recurring_tasks.rb
+++
@@ -1,6 +0,0 @@
-class AddStaticToSolidQueueRecurringTasks < ActiveRecord::Migration[8.0]
-  def change
-    # Agregamos 'default: true' y 'null: false' para evitar problemas
-    add_column :solid_queue_recurring_tasks, :static, :boolean, default: true, null: false
-  end
-end
