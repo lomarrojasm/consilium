29c8d8dbfed1c987293f22cd6d4bd6dc80ec88b9
lomarrojasm <omar.rojas@ligoconsulting.com>
2026-02-17 18:27:12 -0600

    Desactiva SSL para acceso por IP y ajusta Solid Queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,11 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts.clear # Limpia configuraciones prev'ias
   config.hosts << "74.208.227.22"
+  #config.hosts << ".consilium.com" # Si ya tienes dominio
+  config.hosts << /.*\.local/     # Permite hosts internos
+
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   # Code is not reloaded between requests.
@@ -28,10 +32,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
+  config.assume_ssl = false
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
@@ -54,7 +58,7 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  #config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
