9be0aa252041118488eb118b4f3170a346a05e6f
lomarrojasm <omar.rojas@ligoconsulting.com>
2026-06-30 16:59:42 -0600

    fix: cambio de usuarios en seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,37 @@ puts "🏗️ Creando Consultores de Consilium (Admins)..."
 
 # 1. Administrador Principal
 User.create!(
-  first_name: "Omar", 
+  first_name: "Omar",
   last_name: "Rojas",
-  email: "admin@consilium.com", 
+  email: "admin@consilium.com",
   password: "password123",
   password_confirmation: "password123",
-  role: 1, 
-  active: true, 
+  role: 1,
+  active: true,
   job_title: "Socio Director"
 )
 
 # 2. Segundo Administrador
 User.create!(
-  first_name: "Daniel", 
+  first_name: "Daniel",
   last_name: "Burgos",
-  email: "admin2@consilium.com", 
+  email: "negocios@planetafiscal.com",
   password: "password123",
   password_confirmation: "password123",
-  role: 1, 
-  active: true, 
+  role: 1,
+  active: true,
   job_title: "Consultor Senior"
 )
 
 # 3. Consultor Senior
 User.create!(
-  first_name: "Victor", 
+  first_name: "Victor",
   last_name: "Pina",
-  email: "admin3@consilium.com", 
+  email: "victor@planetafiscal.com",
   password: "password123",
   password_confirmation: "password123",
-  role: 1, 
-  active: true, 
+  role: 1,
+  active: true,
   job_title: "Socio Director"
 )
 
